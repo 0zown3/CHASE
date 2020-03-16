9516aa5038e5a65efa5e64990be07720dfb25bc6
0zown3 <0zown3@dev.com>
2020-03-16 00:46:24 -0400

    Add parsing for fireeye threat research blog and return article title matching APT and dtg query

--- cmd/chase/chase.go
+++ cmd/chase/chase.go
@@ -12,5 +12,4 @@ func main() {
 	http.HandleFunc("/", StartHarvest)
 	log.Print("CHASE running @ localhost:7000")
 	log.Fatal(http.ListenAndServe(":7000", nil))
-
 }
--- internal/harvest/harvest_svc.go
+++ internal/harvest/harvest_svc.go
@@ -7,13 +7,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
 )
 
-//Report requires a url string
-type Report struct {
-	URL string
+//Reports requires an array of strings
+type Reports struct {
+	URLS []string
 }
 
 //Domains is the struct used for unmarshalling the allowed_domains json file.
@@ -21,6 +23,22 @@ type Domains struct {
 	Domains []string `json:"domains"`
 }
 
+//FetchReports accepts a dtg and apt string
+func FetchReports(dtg string, apt string) {
+	allowedDomains := loadDomains()
+	for i := 0; i <= len(allowedDomains)-1; i++ {
+		c := colly.NewCollector(
+			colly.AllowedDomains(allowedDomains[i]),
+			colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"),
+		)
+		if allowedDomains[i] == "www.fireeye.com" {
+			url := constructFireeyeURL(dtg, allowedDomains[i])
+			fireeyeHarvester(url, apt, c)
+		}
+	}
+}
+
+//loadDomains loads allowed domains for crawling into memory
 func loadDomains() []string {
 	var domains Domains
 	jsonFile, _ := os.Open("../../config/allowed_domains.json")
@@ -29,20 +47,26 @@ func loadDomains() []string {
 	return domains.Domains
 }
 
-//FetchReports accepts a dtg and apt string
-func FetchReports(dtg string, apt string) {
-	allowedDomains := loadDomains()
-	fmt.Println(allowedDomains)
+func constructFireeyeURL(dtg string, domain string) string {
+	suffix := getFireeyeSuffix(dtg)
+	prefix := "https://" + domain + "/blog/threat-research"
+	return prefix + suffix
 }
 
-// Ping makes a HEAD request to the target URL
-func Ping(reportURL string) int {
-	resp, err := http.Head(reportURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if resp.StatusCode == 200 {
-		return resp.StatusCode
-	}
-	return 0
+func getFireeyeSuffix(dtg string) string {
+	year := dtg[:4]
+	month := dtg[4:6]
+	suffix := "/" + year + "/" + month + ".html"
+	return suffix
+}
+
+func fireeyeHarvester(url string, apt string, c *colly.Collector) {
+	c.OnHTML("h4", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Text, apt) {
+			articleTitle := e.Text
+			fmt.Println(articleTitle)
+		}
+	})
+	log.Printf("Crawling %s for reports relating to %s", url, apt)
+	c.Visit(url)
 }
