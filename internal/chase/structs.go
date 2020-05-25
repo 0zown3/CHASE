1417f4c0ef18707537160e56869b7ab62cebc92f
0zown3 <0zown3@dev.com>
2020-05-25 15:17:54 -0400

    Remove old structs

--- internal/chase/structs.go
+++ internal/chase/structs.go
@@ -1,18 +1,5 @@
 package chase
 
-//RequestBody is the struct for a standard POST request
-type RequestBody struct {
-	APT string
-}
-
-//AliasGroup is the struct for storing a group of APT aliases
-type AliasGroup struct {
-	Groups []string
-}
-
-//APTMappings is the interface for unmarshalling apt_mappings.json
-type APTMappings map[string]AliasGroup
-
 //TRAMRequest is the structure that stores the necessary data to be sent to TRAM
 type TRAMRequest struct {
 	index string //insert_report
